2c1b7bf00caca373d9ffdd32fad5ab9085359587
Laurent BODY <laurent.body@orange.com>
2023-03-31 10:49:16 +0200

    chore(LicensesInventory): add missing headers
    Signed-off-by: Laurent BODY <laurent.body@orange.com>

--- toolbox/LicensesInventory/sources/common/datas.py
+++ toolbox/LicensesInventory/sources/common/datas.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+# Software Name: floss-toolbox
+# SPDX-FileCopyrightText: Copyright (c) 2020-2023 Orange
+# SPDX-License-Identifier: Apache-2.0
+#
+# This software is distributed under the Apache 2.0 license.
+#
+# Author: Laurent BODY <laurent(dot)body(at)orange(dot)com> et al.
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 import sys
 import os
--- toolbox/LicensesInventory/sources/search/parsings.py
+++ toolbox/LicensesInventory/sources/search/parsings.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+# Software Name: floss-toolbox
+# SPDX-FileCopyrightText: Copyright (c) 2020-2023 Orange
+# SPDX-License-Identifier: Apache-2.0
+#
+# This software is distributed under the Apache 2.0 license.
+#
+# Author: Laurent BODY <laurent(dot)body(at)orange(dot)com> et al.
 
 import sys
 import time
